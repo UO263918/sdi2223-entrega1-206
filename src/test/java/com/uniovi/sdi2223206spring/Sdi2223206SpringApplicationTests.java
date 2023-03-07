272780fe6b9a0d3f6007d2adb3a3c1625346e266
Raúl López Calvo <UO263918@uniovi.es>
2023-03-07 07:45:53 +0100

    SDI-2223-206-2.1-Create Spring project.

---
+++ src/test/java/com/uniovi/sdi2223206spring/Sdi2223206SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223206spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223206SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
