a38e085d109c7afc5cb12810800fccece38fe610
Raúl López Calvo <UO263918@uniovi.es>
2023-03-09 07:52:41 +0100

    SDI-2021-206-4.1-SpringBoot-Sesiones

--- src/main/java/com/uniovi/sdi2223206spring/services/MarksService.java
+++ src/main/java/com/uniovi/sdi2223206spring/services/MarksService.java
@@ -3,17 +3,25 @@ package com.uniovi.sdi2223206spring.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
+
 import com.uniovi.sdi2223206spring.entities.Mark;
 import com.uniovi.sdi2223206spring.repositories.MarksRepository;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class MarksService {
     @Autowired
     private MarksRepository marksRepository;
+
+    /* Inyección de dependencias basada en constructor*/
+    private final HttpSession httpSession;
+
+    @Autowired
+    public MarksService(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
 
     public List<Mark> getMarks() {
         List<Mark> marks = new ArrayList<Mark>();
@@ -21,8 +29,15 @@ public class MarksService {
         return marks;
     }
 
-    public Mark getMark(Long id) {
-        return marksRepository.findById(id).get();
+    public Mark getMark(Long id){
+        Set<Mark> consultedList = (Set<Mark>) httpSession.getAttribute("consultedList");
+        if ( consultedList == null ) {
+            consultedList = new HashSet<Mark>();
+        }
+        Mark obtainedMark = marksRepository.findById(id).get();
+        consultedList.add(obtainedMark);
+        httpSession.setAttribute("consultedList", consultedList);
+        return obtainedMark;
     }
 
     public void addMark(Mark mark) {
